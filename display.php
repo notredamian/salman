de0b3594c1773c35be81d28696a912f75f7d2de3
Mohammad Salman <99408156+notredamian@users.noreply.github.com>
2023-12-25 12:14:18 +0600

    Add files via upload

---
+++ display.php
@@ -0,0 +1,71 @@
+<?php
+
+include 'connect.php';
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD operation: Display</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <button class="btn btn-primary my-5"><a href="user.php" class="text-light">Add User</a></button>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Sl No.</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Mobile</th>
+                    <th>Password</th>
+                    <th>Operation</th>
+                </tr>
+            </thead>
+            
+            <tbody>
+
+                <?php
+
+                $sql = "select * from `crud`";
+                $result = mysqli_query($con, $sql);
+
+                if ($result) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $id = $row["id"];
+                        $name = $row['name'];
+                        $email = $row['email'];
+                        $mobile = $row['mobile'];
+                        $password = $row['password'];
+
+                    echo '<tr>
+                    <td>' . $id . '</td>
+                    <td>' . $name . '</td>
+                    <td>' . $email . '</td>
+                    <td>' . $mobile . '</td>
+                    <td>' . $password . '</td>
+                    
+                    <td>
+                    <button class="btn btn-primary"><a href="update.php?updateid=' . $id . '" class="text-light">Update</a></button>
+                    <button class="btn btn-danger"><a href="delete.php?deleteid=' . $id . '" class="text-light">Delete</a></button>
+                    </td>
+                    </tr>';
+                    }
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+
+</body>
+
+</html>
